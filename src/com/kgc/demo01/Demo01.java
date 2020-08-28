722cea750f68773a2a9a4d60fd46f9d6c7d74317
yanglei <784247477@qq.com>
2020-08-28 11:37:38 +0800

    demo01

---
+++ src/com/kgc/demo01/Demo01.java
@@ -0,0 +1,18 @@
+package com.kgc.demo01;
+
+import java.util.Iterator;
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i <50; i++) {
+			
+			System.out.println("ะกร๗..."+i);
+		}
+		
+	
+	}
+	
+
+}
